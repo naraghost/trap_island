a94f544809d110dd75d344413304c8e9c97b4d62
Jeferson 'Shin <shin.jef@gmail.com>
2025-02-14 15:59:36 +0100

    Added start menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,37 +1,47 @@
-extends Control
+class_name MainMenu extends Control
 
-@onready var main_buttons = %MainButtons
-@onready var options_ui = %OptionsUI
-@onready var options_manager = get_node("/root/OptionsManager")
+@onready var campaing_button: TextureButton = %Campagin
+@onready var options_button: TextureButton = %Options
+@onready var extras_button: TextureButton = %Extras
+@onready var achievement_button: TextureButton = %Achievement
+@onready var credits_button: TextureButton = %Credits
+@onready var quit_button: TextureButton = %Quit
 
-func _ready():
-	options_manager.load_options()
-	options_ui.get_node("BackButton").pressed.connect(_on_options_back_pressed)
-	options_ui.get_node("ShakeContainer/ShakeSlider").value_changed.connect(_on_shake_value_changed)
-	_show_main_menu()
 
-func _show_main_menu():
-	main_buttons.show()
-	options_ui.hide()
+func _ready() -> void:
+	campaing_button.grab_focus()
 
-func _show_options():
-	main_buttons.hide()
-	options_ui.show()
-	options_ui.get_node("ShakeContainer/ShakeSlider").value = options_manager.get_screen_shake_intensity()
+	campaing_button.pressed.connect(on_campaing)
+	options_button.pressed.connect(on_options)
+	extras_button.pressed.connect(on_extra)
+	achievement_button.pressed.connect(on_achievement)
+	credits_button.pressed.connect(on_credits)
+	quit_button.pressed.connect(on_quit)
 
-func _on_play_button_pressed():
-	# Change to the main game scene
+
+func on_campaing() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
-func _on_options_button_pressed():
-	_show_options()
 
-func _on_exit_button_pressed():
+func on_options() -> void:
+	push_warning("Not implemented yet")
+	pass
+
+
+func on_extra() -> void:
+	push_warning("Not implemented yet")
+	pass
+
+
+func on_achievement() -> void:
+	push_warning("Not implemented yet")
+	pass
+
+
+func on_credits() -> void:
+	push_warning("Not implemented yet")
+	pass
+
+
+func on_quit() -> void:
 	get_tree().quit()
-
-func _on_options_back_pressed():
-	_show_main_menu()
-
-func _on_shake_value_changed(value: float):
-	options_manager.set_screen_shake_intensity(value)
-	options_manager.save_options()
