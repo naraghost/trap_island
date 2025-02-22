8bd21c6a406fa702f1ddbbac2cfbeef3e10220df
CachorroVoador <87171245+CachorroVoador@users.noreply.github.com>
2025-02-22 02:41:16 -0300

    Simplify menu script

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,47 +1,23 @@
 class_name MainMenu extends Control
 
-@onready var campaing_button: TextureButton = %Campagin
-@onready var options_button: TextureButton = %Options
-@onready var extras_button: TextureButton = %Extras
-@onready var achievement_button: TextureButton = %Achievement
-@onready var credits_button: TextureButton = %Credits
-@onready var quit_button: TextureButton = %Quit
-
+@export var button_container: Control
 
 func _ready() -> void:
-	campaing_button.grab_focus()
+	#Get all children that inherit from BaseButton
+	var buttons := button_container.find_children('', 'BaseButton')
+	buttons[0].grab_focus()
+	
+	for button: BaseButton in buttons:
+		#Check if a method with the button's name exists in this script
+		if not has_method(button.name):
+			#Otherwise, print a warning
+			push_warning('%s is not implemented yet' % button.name)
+			continue
+		#Connect the button's clicked signal to a method with the same name
+		button.button_up.connect(Callable(self, button.name))
 
-	campaing_button.pressed.connect(on_campaing)
-	options_button.pressed.connect(on_options)
-	extras_button.pressed.connect(on_extra)
-	achievement_button.pressed.connect(on_achievement)
-	credits_button.pressed.connect(on_credits)
-	quit_button.pressed.connect(on_quit)
-
-
-func on_campaing() -> void:
+func campaign() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
-
-func on_options() -> void:
-	push_warning("Not implemented yet")
-	pass
-
-
-func on_extra() -> void:
-	push_warning("Not implemented yet")
-	pass
-
-
-func on_achievement() -> void:
-	push_warning("Not implemented yet")
-	pass
-
-
-func on_credits() -> void:
-	push_warning("Not implemented yet")
-	pass
-
-
-func on_quit() -> void:
+func quit() -> void:
 	get_tree().quit()
